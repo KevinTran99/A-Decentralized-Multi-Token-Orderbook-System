219767452a64f23e7e40ea87f541d61d12d03d7c
KevinTran99 <kevintrann99@gmail.com>
2024-12-20 21:32:57 +0100

    Update MockBTC to use correct decimal places

--- blockchain/ignition/modules/MockBTCModule.ts
+++ blockchain/ignition/modules/MockBTCModule.ts
@@ -1,7 +1,7 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 const MockBTCModule = buildModule('MockBTCModule', m => {
-  const mockBTC = m.contract('ERC20Mock', ['Bitcoin', 'BTC', 18]);
+  const mockBTC = m.contract('ERC20Mock', ['Bitcoin', 'BTC', 8]);
 
   return { mockBTC };
 });
