0ccfef34556d50683a78a60bff691a273c97b19f
KevinTran99 <kevintrann99@gmail.com>
2024-12-26 19:25:07 +0100

    Add decimals to token configuration

--- backend/src/config/tokens.mjs
+++ backend/src/config/tokens.mjs
@@ -2,9 +2,11 @@ export const SUPPORTED_TOKENS = [
   {
     address: process.env.BTC_ADDRESS,
     symbol: 'BTC',
+    decimals: 8,
   },
   {
     address: process.env.ETH_ADDRESS,
     symbol: 'ETH',
+    decimals: 18,
   },
 ];
